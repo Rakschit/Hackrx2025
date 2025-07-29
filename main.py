d96082fd22016a0a22947c590f9ca1f9969ac326
unknown <rakschit034khalkho@gmail.com>
2025-07-29 11:30:13 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,44 +1,6 @@
-from fastapi import FastAPI, File, UploadFile
-import shutil
-import os
-from file_utils import extract_text_from_pdf, clean_text, chunk_text
-from embeddings_utils import create_embeddings
+from fastapi import FastAPI
+
 app = FastAPI()
-
-@app.post("/hackrx/run")
-async def upload_file(file: UploadFile = File(...)):
-    
-    file_type =  os.path.splitext(file.filename)[1].lower()
-    allowed = [".pdf", ".docx", ".eml"]
-
-    if file_type not in allowed:
-        return {"Error: Invalid file type \nOnly accepts pdf,docx and eml files"}
-    
-    temp_path = f"/tmp/{file.filename}"
-    with open(temp_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
-    
-    size = os.path.getsize(temp_path)
-    
-    if file_type == ".pdf":
-        file_content = extract_text_from_pdf(temp_path)
-
-    file_content = clean_text(file_content)
-    chunks = chunk_text(file_content)
-    index_path, chunks_path = create_embeddings(chunks)
-
-    try:
-        os.remove(temp_path)
-    except Exception as ae:
-        print(f"Failed to delete temp file:{ae}")
-
-    return{
-        "filename": file.filename,
-        "size": size,
-        "text": file_content[:21],
-        "index": index_path,
-        "chunks": chunks_path
-    }
 
 @app.post("/hackrx/hello")
 async def run():
