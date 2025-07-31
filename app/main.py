03c0171fddc50039c15ea1b1c02c2cba242d8d71
unknown <rakschit034khalkho@gmail.com>
2025-07-31 14:19:09 +0530

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from pathlib import Path
+import nltk
+from nltk.tokenize import sent_tokenize
 
 app = FastAPI()
 
 @app.post("/")
 def read_root():
-    return {"message": "Hello World"}
+    nltk.data.path.append(str(Path(__file__).resolve().parent.parent / "nltk_data"))
+    text = "Hello Railway! Tokenization works offline."
+    sent_tokenize(text)
+    
