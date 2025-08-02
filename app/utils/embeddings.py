3cf554c0e5c6deaf3134c78f2b0ff0464b59762c
unknown <rakschit034khalkho@gmail.com>
2025-08-02 14:13:10 +0530

    Initial commit

--- app/utils/embeddings.py
+++ app/utils/embeddings.py
@@ -15,7 +15,7 @@ def get_pinecone_index():
     if index_name not in pc.list_indexes().names():
         pc.create_index(
             name=index_name,
-            dimension=768,
+            dimension=3072,
             metric="cosine",
             spec=ServerlessSpec(cloud="aws", region="us-east-1")
         )
